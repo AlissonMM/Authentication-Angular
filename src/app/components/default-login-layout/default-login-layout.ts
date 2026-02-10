0300a1db9f4269539a6429cc589641665ce40d20
AlissonMM <alissonmmedeji@gmail.com>
2026-02-10 20:10:13 -0300

    feat: Refactor login layout and styles to use SCSS with improved structure and inputs

--- src/app/components/default-login-layout/default-login-layout.ts
+++ src/app/components/default-login-layout/default-login-layout.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
   imports: [],
   templateUrl: './default-login-layout.html',
-  styleUrl: './default-login-layout.css',
+  styleUrl: './default-login-layout.scss',
 })
 export class DefaultLoginLayout {
+
+  @Input() title: string = "";
+
+  @Input() primaryBtnText: string = "";
+
+  @Input() secondaryBtnText: string = "";
 
 }
